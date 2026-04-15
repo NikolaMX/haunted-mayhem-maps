9f2eeb2b5495a7c767e8a7c6d7207fd59d3e4bfc
NikolaMX <nikola.mixailov@abv.bg>
2026-04-15 11:21:44 +0300

    initial

---
+++ ui/mods/com.pa.nik.haunted-mayhem-maps/maplist.js
@@ -0,0 +1,9 @@
+cShareSystems.load_pas("Haunted Mayhem Mode", [
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/alloy_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/roc_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/superbowl_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/lock_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/amplus_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/mukden_haunted_mayhem.pas",
+	"coui://ui/mods/com.pa.nik.haunted-mayhem-maps/systems/shoreline_haunted_mayhem.pas"
+]);
